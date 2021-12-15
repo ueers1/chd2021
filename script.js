1702f74ae69c7e00f12ba320f888e8995caab631
ueers1 <89825696+ueers1@users.noreply.github.com>
2021-12-15 12:08:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+
+function renderBoard(numRows, numCols, grid) {
+    let boardEl = document.querySelector("#board");
+
+    for (let i = 0; i < numRows; i++) {
+        let trEl = document.createElement("tr");
+        for (let j = 0; j < numCols; j++) {
+            let cellEl = document.createElement("div");
+            cellEl.className = "cell";
+            grid[i][j].cellEl = cellEl;
+
+            // if ( grid[i][j].count === -1) {
+            //     cellEl.innerText = "*";    
+            // } else {
+            //     cellEl.innerText = grid[i][j].count;
+            // }
+
+            cellEl.addEventListener("click", (e)=> {
+                if (grid[i][j].count === -1) {
+                    explode(grid, i, j, numRows, numCols)
+                    return;
+                }
+
+                if (grid[i][j].count === 0 ) {
+                    searchClearArea(grid, i, j, numRows, numCols);
+                } else if (grid[i][j].count > 0) {
+                    grid[i][j].clear = true;
+                    cellEl.classList.add("clear");
+                    grid[i][j].cellEl.innerText = grid[i][j].count;
+                }
+
+                checkAllClear(grid);
+                // cellEl.classList.add("clear");
+            });
+
+            let tdEl = document.createElement("td");
+            tdEl.append(cellEl);
+
+            trEl.append(tdEl);
+        }
+        boardEl.append(trEl);
+    }
+}
+
+const directions = [
+    [-1, -1], [-1, 0], [-1, 1], // TL, TOP, TOP-RIGHT
+    [0, -1], [0, 1],
+    [1, -1], [1, 0], [1, 1],
+]
+
+
+function initialize(numRows, numCols, numMines) {
+    let grid = new Array(numRows);
+    for (let i = 0; i < numRows; i++) {
+        grid[i] = new Array(numCols);
+        for (let j = 0; j < numCols; j++) {
+            grid[i][j] = {
+                clear: false,
+                count: 0
+            };
+        }
+    }
+    let mines = [];
+    for (let k = 0; k < numMines; k++) {
+        let cellSn = Math.trunc(Math.random() * numRows * numCols);
+        let row = Math.trunc(cellSn / numCols);
+        let col = cellSn % numCols;
+
+        console.log(cellSn, row, col);
+
+        grid[row][col].count = -1;
+        mines.push([row, col]);
+    }
+    // 计算有雷的周边为零的周边雷数
+    for (let [row, col] of mines) {
+        console.log("mine: ", row, col);
+        for (let [drow, dcol] of directions) {
+            let cellRow = row + drow;
+            let cellCol = col + dcol;
+            if (cellRow < 0 || cellRow >= numRows || cellCol < 0 || cellCol >= numCols) {
+                continue;
+            }
+            if (grid[cellRow][cellCol].count === 0) {
+                console.log("target: ", cellRow, cellCol);
+
+                let count = 0;
+                for (let [arow, acol] of directions) {
+                    let ambientRow = cellRow + arow;
+                    let ambientCol = cellCol + acol;
+                    if (ambientRow < 0 || ambientRow >= numRows || ambientCol < 0 || ambientCol >= numCols) {
+                        continue;
+                    }
+
+                    if (grid[ambientRow][ambientCol].count === -1) {
+                        console.log("danger!", ambientRow, ambientCol);
+                        count += 1;
+                    }
+                }
+
+                if (count > 0) {
+                    grid[cellRow][cellCol].count = count;
+                }
+            }
+        }
+
+    }
+    
+    // console.log(grid);
+
+    return grid;
+}
+
+
+
+let grid = initialize(9, 9, 9);
+
+
+renderBoard(9, 9, grid);
